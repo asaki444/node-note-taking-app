a273b2f2558f11cc4cd88097b8ad23d90bf3ddfd
Aditi <asaki444@gmail.com>
2020-06-19 23:51:39 -0400

    added yargs npm package, and utilized command feature to list commands in the terminal

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 const validator = require('validator')
-
+const yargs = require('yargs')
 const chalk = require('chalk');
 
-const command = process.argv[2];
+yargs.version('1.1.0');
 
+// Create add command
 
-if(command === 'add'){
-    console.log('Adding a note!')
-}
-else if(command === 'remove'){
-   console.log('Removing note!')
-}
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function (){
+        console.log('Adding a new note')
+    }
+})
 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note!')
+    }
+
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function(){
+        console.log('read a note')
+    }
+
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list notes',
+    handler: function(){
+        console.log('list all the notes')
+    }
+
+})
+//add , remove, read, list 
+
+console.log(yargs.argv)
 
