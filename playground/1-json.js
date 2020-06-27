f0cb593c27dc64f73fdb600d267554423603a924
Aditi <asaki444@gmail.com>
2020-06-26 23:39:30 -0400

    took out commented code

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,13 +1,9 @@
 const fs = require('fs')
 
-// const book = {
-//     'title': 'Ego is the Enemy',
-//     'author': 'Ryan Holiday'
-// }
-
-// JSON.stringify(book)
 
 const dataBuffer = fs.readFileSync('1-json.json');
 const dataJSON = dataBuffer.toString();
 const data = JSON.parse(dataJSON)
-console.log(data.title)
+data.name = "aditi";
+data.age = 88;
+console.log(data)
